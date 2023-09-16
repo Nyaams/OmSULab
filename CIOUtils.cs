f75a42ec394a8245025730b6b97e9a1ae18d9537
Nyaams <lemmingw@gmail.com>
2023-09-16 16:16:53 +0600

    Input/Output Utils

---
+++ CIOUtils.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmSULab
+{
+    internal class CIOUtils
+    {
+        public static int iSafeRead()
+        {
+            while (true)
+            {
+                string sValue = Console.ReadLine();
+                int iValue = 0;
+                if (Int32.TryParse(sValue, out iValue))
+                    return iValue;
+                Console.Write("Неверный формат. Введите целочисленное значение: ");
+            }
+        }
+        public static double dSafeRead()
+        {
+            while (true)
+            {
+                string sAmount = Console.ReadLine();
+                double dAmount = 0;
+                if (Double.TryParse(sAmount, out dAmount))
+                    return dAmount;
+                Console.Write("Неверный формат. Введите числовое значение: ");
+            }
+        }
+    }
+}

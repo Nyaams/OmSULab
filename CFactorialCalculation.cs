f76fbc7e2b840cf2ac8050d5740fa1d77c19bd92
Nyaams <lemmingw@gmail.com>
2023-09-16 20:23:44 +0600

    task2

---
+++ CFactorialCalculation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmSULab
+{
+    internal class CFactorialCalculation
+    {
+        public static int iFactorialCalc(int n)
+        {
+            if (n == 0 || n == 1) return 1;
+            return n * iFactorialCalc(n - 1);
+        }
+    }
+}

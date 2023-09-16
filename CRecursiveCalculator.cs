d1b57b0873eb166c03b4f12470c2d391aafd860e
Nyaams <lemmingw@gmail.com>
2023-09-16 19:20:04 +0600

    task2

---
+++ CRecursiveCalculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmSULab
+{
+    internal class CRecursiveCalculator
+    {
+        public static int iDateRequest()
+        {
+            Console.Write("Введите количество вводимых дат: ");
+            int iLimitDates = CIOUtils.iSafeRead();
+            DateTime[] inputDate = new DateTime[iLimitDates + 1];
+            if (iLimitDates % 2 == 0)
+            {
+                for (int i = 0; i < iLimitDates; i++)
+                {
+                    Console.Write($"Введите {i+1} дату в формате (дд.мм.гггг): ");
+                    inputDate[i] = CIOUtils.dtSafeRead();
+                    Console.WriteLine(inputDate[i].ToShortDateString());
+                }
+            }
+            else
+            {
+                Console.Write("Количество дат должно быть чётным.\n" +
+                    "Введите количество снова: ");
+                iLimitDates = CIOUtils.iSafeRead();
+            }      
+            return 0;
+        }
+    }
+}

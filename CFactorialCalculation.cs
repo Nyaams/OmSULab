703fc5b4a394f2fa51d157d6a0a9b757dd2bb17b
Nyaams <lemmingw@gmail.com>
2023-09-23 19:05:58 +0600

    useless correction

--- CFactorialCalculation.cs
+++ CFactorialCalculation.cs
@@ -8,12 +8,12 @@ namespace OmSULab
 {
     internal class CFactorialCalculation
     {
-        public static double iFactorialCalc(double n)
+        public static double dFactorialCalc(double n)
         {
             if (n == 0 || n == 1) 
                 return 1;
 
-            return n * iFactorialCalc(n - 1);
+            return n * dFactorialCalc(n - 1);
         }
     }
 }

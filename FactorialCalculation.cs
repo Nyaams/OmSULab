b02c45224c24a44263412737709a65776196b7b8
Nyaams <lemmingw@gmail.com>
2023-09-28 20:25:15 +0600

    task2 1.1

---
+++ FactorialCalculation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmSULab
+{
+    internal class FactorialCalculation
+    {
+        public static double dFactorialCalc(double n)
+        {
+            if (n == 0 || n == 1) 
+                return 1;
+
+            return n * dFactorialCalc(n - 1);
+        }
+    }
+}

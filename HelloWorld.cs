3472bf901418ad04d5d176748acba654260dcce8
Nyaams <lemmingw@gmail.com>
2023-09-28 21:53:05 +0600

    ?

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmSULab
+{
+    internal class HelloWorld
+    {
+        public static void Hello()
+        {
+            Console.WriteLine("Привет Мир!");
+            Console.WriteLine("Hello World!");            
+            Console.WriteLine("Hallo Welt!");
+            Console.WriteLine("Bonjour le monde!");
+        }
+    }
+}

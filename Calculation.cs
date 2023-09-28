a9cf416536b2220204fed8aee137c31ccf80c9b2
Nyaams <lemmingw@gmail.com>
2023-09-29 00:22:04 +0600

    task1 1.1

--- Calculation.cs
+++ Calculation.cs
@@ -6,34 +6,38 @@ using System.Threading.Tasks;
 
 namespace OmSULab
 {
-    internal class CalculationByTheFormula
+    internal class Calculation
     {
-        public static double dCalculation()
+        private static double argX;
+        private static double argY;
+        private static double argZ;
+        private static double dCalculationArguments()
         {
             Console.Write("Введите аргумент х: ");
-            double argX = CIOUtils.dSafeRead();
+            argX = CIOUtils.dSafeRead();
             while (argX < 0)
             {
                 Console.Write("Квадратный корень из отрицательных чисел не является вещественным.\n" +
                               "Введите новое значение аргумента х: ");
                 argX = CIOUtils.dSafeRead();
             }
-
             Console.Write("Введите аргумент y: ");
-            double argY = CIOUtils.dSafeRead();
-
+            argY = CIOUtils.dSafeRead();
             Console.Write("Введите аргумент z: ");
-            double argZ = CIOUtils.dSafeRead();
             while (argZ == 0)
             {
                 Console.Write("Деление на 0 невозможно\nВведите новое значение аргумента z: ");
                 argZ = CIOUtils.dSafeRead();
             }
-
+            argZ = CIOUtils.dSafeRead();
+            return 0;
+        }
+        public static void dCalculation()
+        {
+            dCalculationArguments();
             double dResult;
             dResult = (argY - Math.Sqrt(argX)) / argZ;
             Console.WriteLine("(y-√(x))/z = " + Math.Round(dResult, 3));
-            return 0;
         }
     }
 }

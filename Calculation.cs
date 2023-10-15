e4c91d48b601ae04a98ef92fb65eb5843e77f772
Nyaams <lemmingw@gmail.com>
2023-10-15 20:52:05 +0600

    task1 1.2

--- Calculation.cs
+++ Calculation.cs
@@ -6,37 +6,42 @@ using System.Threading.Tasks;
 
 namespace OmSULab
 {
-    internal class Calculation
+    internal class Calculation : MenuItemCore
     {
-        private static double argX;
-        private static double argY;
-        private static double argZ;
+        public override string GetTitle()
+        {
+            return "[2] Calc: (y-√(x))/z";
+        }
+        private static double dArgumentX;
+        private static double dArgumentY;
+        private static double dArgumentZ;
         private static double dCalculationArguments()
         {
             Console.Write("Введите аргумент х: ");
-            argX = CIOUtils.dSafeRead();
-            while (argX < 0)
+            dArgumentX = CIOUtils.dSafeRead();
+            while (dArgumentX < 0)
             {
                 Console.Write("Квадратный корень из отрицательных чисел не является вещественным.\n" +
                               "Введите новое значение аргумента х: ");
-                argX = CIOUtils.dSafeRead();
+                dArgumentX = CIOUtils.dSafeRead();
             }
             Console.Write("Введите аргумент y: ");
-            argY = CIOUtils.dSafeRead();
+            dArgumentY = CIOUtils.dSafeRead();
             Console.Write("Введите аргумент z: ");
-            while (argZ == 0)
+            dArgumentZ = CIOUtils.dSafeRead();
+            while (dArgumentZ == 0)
             {
                 Console.Write("Деление на 0 невозможно\nВведите новое значение аргумента z: ");
-                argZ = CIOUtils.dSafeRead();
-            }
-            argZ = CIOUtils.dSafeRead();
+                dArgumentZ = CIOUtils.dSafeRead();
+            }            
             return 0;
         }
-        public static void dCalculation()
+        public override void Execute()
         {
+            Console.Clear();
             dCalculationArguments();
             double dResult;
-            dResult = (argY - Math.Sqrt(argX)) / argZ;
+            dResult = (dArgumentY - Math.Sqrt(dArgumentX)) / dArgumentZ;
             Console.WriteLine("(y-√(x))/z = " + Math.Round(dResult, 3));
         }
     }

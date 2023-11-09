cb03becdff019a0ff41c31047f879f961a6ed921
Nyaams <lemmingw@gmail.com>
2023-11-09 23:31:24 +0600

    task3 2.0

--- Strings.cs
+++ Strings.cs
@@ -16,82 +16,123 @@ namespace OmSULab
         }
         static void task3()
         {
-            Console.WriteLine("Введите первую строку: ");
-            string sFirstString = CIOUtils.sSafeRead();            
-            Console.Clear();
+            CIOUtils.printfn("Введите первую строку: ");
+            string sFirstString = CIOUtils.sSafeRead();
 
-            Console.WriteLine("Введите вторую строку: ");
+            CIOUtils.printfn("Введите вторую строку: ");
             string sSecondString = CIOUtils.sSafeRead();
             Console.Clear();
 
-            Console.WriteLine("Первая строка: " + sFirstString + "\n" + "Вторая строка: " + sSecondString);
-            if (sFirstString == sSecondString) 
-            { 
-                Console.WriteLine("\nСовпадают ли они посимвольно? - Совпадают."); 
+            CIOUtils.printfn("Первая строка: " + sFirstString + "\n" + "Вторая строка: " + sSecondString);
+            try
+            {
+                Compare(sFirstString, sSecondString);
+                CIOUtils.printfn("\nСовпадают ли они посимвольно? - Совпадают.");
             }
-            else 
-            { 
-                Console.WriteLine("\nСовпадают ли они посимвольно? - Не совпадают."); 
-            }
-            sFirstString = sFirstString.Replace("  ", " ");            
+            catch (ValidationException firstEx) { CIOUtils.printfn(firstEx.Message); }
+
+            string sFirstStringForRegExp = sFirstString;
+            string sSecondStringForRegExp = sSecondString;
+            sFirstString = sFirstString.Replace("  ", " ");
             sFirstString = sFirstString.ToUpper();
             sFirstString = sFirstString.Trim();
             sSecondString = sSecondString.Replace("  ", " ");
             sSecondString = sSecondString.ToUpper();
             sSecondString = sSecondString.Trim();
-            string sFirstStringForRegExp = sFirstString;
-            string sSecondStringForRegExp = sSecondString;
 
-            if (sFirstString == sSecondString)
+            try
             {
-                Console.WriteLine("Совпадают ли они посимвольно с модификациями? - Совпадают."); 
+                ModificationCompare(sFirstString, sSecondString);
+                CIOUtils.printfn("Совпадают ли они посимвольно с модификациями? - Совпадают.");
             }
-            else 
-            { 
-                Console.WriteLine("Совпадают ли они посимвольно с модификациями? - Не совпадают."); 
-            }
+            catch (ValidationException secondEx) { CIOUtils.printfn(secondEx.Message); }
 
-            string sFirstModifiedString = new string(sFirstString.Reverse().ToArray());
-            if (sFirstModifiedString == sSecondString)
+            string sFirstReversedString = new string(sFirstString.Reverse().ToArray());
+            try
             {
-                Console.WriteLine("Является ли одна строка перевёртышем к другой? - Является.\n");
+                ReverseCompare(sFirstReversedString, sSecondString);
+                CIOUtils.printfn("Является ли одна строка перевёртышем к другой? - Является.\n");
             }
-            else
-            {
-                Console.WriteLine("Является ли одна строка перевёртышем к другой? - Не является.\n");
-            }
+            catch (ValidationException thirdEx) { CIOUtils.printfn(thirdEx.Message); }
 
+            CIOUtils.printfn(sFirstStringForRegExp);
+            RegexCheckEmail(sFirstStringForRegExp);
+            RegexCheckPhone(sFirstStringForRegExp);
+            RegexCheckIP(sFirstStringForRegExp);
+
+            CIOUtils.printfn("\n" + sSecondStringForRegExp);
+            RegexCheckEmail(sSecondStringForRegExp);
+            RegexCheckPhone(sSecondStringForRegExp);
+            RegexCheckIP(sSecondStringForRegExp);
+        }
+
+        public static bool Compare(string firstString, string secondString)
+        {
+            if (firstString == secondString) return true;
+            throw new ValidationException("\nСовпадают ли они посимвольно? - Не совпадают.");
+        }
+        public static bool ModificationCompare(string firstString, string secondString)
+        {
+            if (firstString == secondString) return true;
+            throw new ValidationException("Совпадают ли они посимвольно с модификациями? - Не совпадают.");
+        }
+        public static bool ReverseCompare(string firstString, string secondString)
+        {
+            if (firstString == secondString) return true;
+            throw new ValidationException("Является ли одна строка перевёртышем к другой? - Не является.\n");
+        }
+        public static void RegexCheckEmail(string str)
+        {
+            try
+            {
+                isEmail(str);
+                CIOUtils.printfn("Строка является Email'ом");
+                return;
+            }
+            catch (ValidationException EmailEx) { CIOUtils.printfn(EmailEx.Message); }
+        }
+        public static void RegexCheckPhone(string str)
+        {
+            try
+            {
+                isPhone(str);
+                CIOUtils.printfn("Строка является Тел. номером");
+                return;
+            }
+            catch (ValidationException PhoneEx) { CIOUtils.printfn(PhoneEx.Message); }
+        }
+        public static void RegexCheckIP(string str)
+        {
+            try
+            {
+                isIP(str);
+                CIOUtils.printfn("Строка является IP-адресом");
+                return;
+            }
+            catch (ValidationException IPEx) { CIOUtils.printfn(IPEx.Message); }
+        }
+        public static bool isEmail(string str)
+        {
             string sEmailPattern = "[^@ \\t\\r\\n]+@[^@ \\t\\r\\n]+\\.[^@ \\t\\r\\n]+";
-            if (Regex.IsMatch(sFirstStringForRegExp, sEmailPattern))
-                Console.WriteLine("Первая строка является Email'ом: " + sFirstStringForRegExp);
-            else { Console.WriteLine("Первая строка не является Email'ом"); }
-            
+            if (Regex.IsMatch(str, sEmailPattern)) { return true; }
+            throw new ValidationException("Строка не является Email'ом");
+        }
+        public static bool isPhone(string str)
+        {
             string sPhonePattern = "^[\\+]?[(]?[0-9]{3}[)]?[-\\s\\.]?[0-9]{3}[-\\s\\.]?[0-9]{4,6}$";
-            if (Regex.IsMatch(sFirstStringForRegExp, sPhonePattern))
-                Console.WriteLine("Первая строка является Тел. номером: " + sFirstStringForRegExp);
-            else { Console.WriteLine("Первая строка не является Тел. номером"); }
-            
-            string sIPAddress = "(\\b25[0-5]|\\b2[0-4][0-9]|\\b[01]?[0-9][0-9]?)(\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}";
-            if (Regex.IsMatch(sFirstStringForRegExp, sIPAddress))
-                Console.WriteLine("Первая строка является IP-адресом: " + sFirstStringForRegExp + "\n");
-            else { Console.WriteLine("Первая строка не является IP-адресом\n"); }
-
-            if (Regex.IsMatch(sSecondStringForRegExp, sEmailPattern))
-                Console.WriteLine("Вторая строка является Email'ом: " + sSecondStringForRegExp);
-            else { Console.WriteLine("Вторая строка не является Email'ом"); }            
-           
-            if (Regex.IsMatch(sSecondStringForRegExp, sPhonePattern))
-                Console.WriteLine("Вторая строка является Тел. номером: " + sSecondStringForRegExp);
-            else { Console.WriteLine("Вторая строка не является Тел. номером"); }            
-            
-            if (Regex.IsMatch(sSecondStringForRegExp, sIPAddress))
-                Console.WriteLine("Вторая строка является IP-адресом: " + sSecondStringForRegExp);
-            else { Console.WriteLine("Вторая строка не является IP-адресом\n"); }
+            if (Regex.IsMatch(str, sPhonePattern)) { return true; }
+            throw new ValidationException("Строка не является Тел. номером");
+        }
+        public static bool isIP(string str)
+        {
+            string sIPAddressPattern = "(\\b25[0-5]|\\b2[0-4][0-9]|\\b[01]?[0-9][0-9]?)(\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}";
+            if (Regex.IsMatch(str, sIPAddressPattern)) { return true; }
+            throw new ValidationException("Строка не является IP-адресом");
         }
         public override void Execute()
         {
             Console.Clear();
-            Strings.task3();
+            task3();
         }
     }
 }

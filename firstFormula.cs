a8f188da49b93c24f259071fca9eb1a971cb2e7f
Nyaams <lemmingw@gmail.com>
2023-11-23 17:10:31 +0600

    task5 1.0

---
+++ firstFormula.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmSULab
+{
+	internal class firstFormula : Calculation
+	{
+		public static void Calc()
+		{
+			double dResult;
+			dResult = (Calculation.ArgumentY() - Math.Sqrt(Calculation.ArgumentX())) / Calculation.ArgumentZ();
+			CIOUtils.printfn("(y-√(x))/z = " + Math.Round(dResult, 3));
+		}
+	}
+}

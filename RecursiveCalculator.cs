3034ad482fc417881db443a74dd368952e10f658
Nyaams <lemmingw@gmail.com>
2023-12-21 23:27:09 +0600

    task2 2.3

--- RecursiveCalculator.cs
+++ RecursiveCalculator.cs
@@ -5,12 +5,13 @@ using System.Globalization;
 using System.Linq;
 using System.Net.NetworkInformation;
 using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OmSULab
 {
-    internal class RecursiveCalculator
+    public class RecursiveCalculator
     {
         static int iLimitDates = 4;
         static DateTime[] inputDate = new DateTime[iLimitDates + 1];
@@ -37,11 +38,65 @@ namespace OmSULab
                 Console.Write("Конечная дата не может быть меньше начальной.\n" +
                               "Введите другое значение конечной даты во втором отрезке: ");
                 inputDate[3] = CIOUtils.dtSafeRead();
-            }
+            }        
             return 0;
         }
-        static double dSum = 0;
-        public static double dDateCalculation()
+        static double dResTest = 0;
+        public static double dDateCalculation(DateTime firstDate, DateTime secondDate, DateTime thirdDate, DateTime fourthDate)
+        {            
+            CIOUtils.printfn("Первый отрезок дат: " + firstDate.ToShortDateString() +
+                              "-" + secondDate.ToShortDateString());
+
+            CIOUtils.printfn("Второй отрезок дат: " + thirdDate.ToShortDateString() +
+                              "-" + fourthDate.ToShortDateString());
+
+            if (thirdDate > secondDate ||
+                fourthDate < firstDate)
+            {
+                CIOUtils.printfn("Отрезки дат не пересекаются.");
+                dResTest = 0;
+            }
+            else if (thirdDate > firstDate &&
+                     fourthDate > secondDate)
+                dResTest = (secondDate - thirdDate).TotalDays + 1;
+
+            else if (thirdDate < firstDate &&
+                     fourthDate < secondDate)
+                dResTest = (fourthDate - firstDate).TotalDays + 1;
+
+            else if (thirdDate > firstDate &&
+                     fourthDate < secondDate)
+                dResTest = (fourthDate - firstDate).TotalDays;
+
+            else if (thirdDate > firstDate &&
+                     fourthDate == secondDate)
+                dResTest = (fourthDate - thirdDate).TotalDays + 1;
+
+            else if (thirdDate == firstDate &&
+                     fourthDate < secondDate)
+                dResTest = (fourthDate - thirdDate).TotalDays + 1;
+
+            else if (thirdDate < firstDate &&
+                     fourthDate > secondDate)
+                dResTest = (secondDate - firstDate).TotalDays + 1;
+
+            else dResTest = (fourthDate - firstDate).TotalDays + 1;
+
+            while (dResTest > 170)
+            {
+                CIOUtils.printfn("Значение аргумента больше 170 и не может быть вычислено за отведенное время.\n" +
+                                  "Введите данные повторно.");
+                return dDateRequest();
+
+            }            
+            return dResTest;
+        }
+        public static double outRes()
+        {
+            return dResTest;
+        }
+        static double dRes = 0;
+        public static double dDateResult()
         {
             Console.Clear();
             CIOUtils.printfn("Первый отрезок дат: " + inputDate[0].ToShortDateString() +
@@ -54,52 +109,53 @@ namespace OmSULab
                 inputDate[3] < inputDate[0])
             {
                 CIOUtils.printfn("Отрезки дат не пересекаются.");
-                dSum = 0;
+                dRes = 0;
             }
             else if (inputDate[2] > inputDate[0] &&
                      inputDate[3] > inputDate[1])
-                dSum = (inputDate[1] - inputDate[2]).TotalDays + 1;
+                dRes = (inputDate[1] - inputDate[2]).TotalDays + 1;
 
             else if (inputDate[2] < inputDate[0] &&
                      inputDate[3] < inputDate[1])
-                dSum = (inputDate[3] - inputDate[0]).TotalDays + 1;
+                dRes = (inputDate[3] - inputDate[0]).TotalDays + 1;
 
             else if (inputDate[2] > inputDate[0] &&
                      inputDate[3] < inputDate[1])
-                dSum = (inputDate[3] - inputDate[0]).TotalDays;
+                dRes = (inputDate[3] - inputDate[0]).TotalDays;
 
             else if (inputDate[2] > inputDate[0] &&
                      inputDate[3] == inputDate[1])
-                dSum = (inputDate[3] - inputDate[2]).TotalDays + 1;
+                dRes = (inputDate[3] - inputDate[2]).TotalDays + 1;
 
             else if (inputDate[2] == inputDate[0] &&
                      inputDate[3] < inputDate[1])
-                dSum = (inputDate[3] - inputDate[2]).TotalDays + 1;
+                dRes = (inputDate[3] - inputDate[2]).TotalDays + 1;
 
             else if (inputDate[2] < inputDate[0] &&
                      inputDate[3] > inputDate[1])
-                dSum = (inputDate[1] - inputDate[0]).TotalDays + 1;
+                dRes = (inputDate[1] - inputDate[0]).TotalDays + 1;
 
-            else dSum = (inputDate[3] - inputDate[0]).TotalDays + 1;
+            else dRes = (inputDate[3] - inputDate[0]).TotalDays + 1;
 
-            while (dSum > 170)
+            while (dRes > 170)
             {
                 CIOUtils.printfn("Значение аргумента больше 170 и не может быть вычислено за отведенное время.\n" +
                                   "Введите данные повторно.");
                 return dDateRequest();
 
             }
-            return dSum;
+            return dRes;
         }
-        public static double dDateMethodsCall()
+        public static void Exe()
         {
+            Console.Clear();
             dDateRequest();
-            dDateCalculation();
-            return 0;
-        }
-        public static double dDateResult()
-        {
-            return dSum;
+            dDateResult();
+            var sw = new Stopwatch();
+            sw.Start();
+            secondFormula.Calc(dRes);
+            sw.Stop();
+            CIOUtils.printf($"Время потраченное на вычисление факториала: {sw.Elapsed} \n");
         }
     }
 }

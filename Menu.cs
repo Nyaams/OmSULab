333d552abeb29ac92c5ae073758f595cc435d4b3
Nyaams <lemmingw@gmail.com>
2023-12-21 23:26:37 +0600

    2.2

--- Menu.cs
+++ Menu.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace OmSULab
 {
-    static class Menu
+    public static class Menu
     {
         static List<MenuItemCore> Items = new List<MenuItemCore>();
         public static int iMenuItems

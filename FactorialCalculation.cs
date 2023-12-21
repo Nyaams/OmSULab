eacdeba204039dcbc6d11a971facc79b2af2b806
Nyaams <lemmingw@gmail.com>
2023-12-21 23:26:17 +0600

    task2 2.4

--- FactorialCalculation.cs
+++ FactorialCalculation.cs
@@ -7,21 +7,8 @@ using System.Threading.Tasks;
 
 namespace OmSULab
 {
-    internal class FactorialCalculation : MenuItemCore
-    {
-        public override string GetTitle()
-        {
-            return "[3] Recursion date";
-        }
-        public override void Execute()
-        {
-            Console.Clear();
-            RecursiveCalculator.dDateMethodsCall();
-            var sw = new Stopwatch();
-            sw.Start();
-            secondFormula.Calc();
-            sw.Stop();
-            CIOUtils.printf($"Время потраченное на вычисление факториала: {sw.Elapsed} \n");
-        }
+    public class FactorialCalculation
+    {       
+        
     }
 }
